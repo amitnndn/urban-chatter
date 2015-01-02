915a52852f5a3d0ed3f2762fed136aa9758671fa
Siddhanth Shetty <sishetty@akamai.com>
2015-01-02 12:22:17 +0530

    bug fixes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -266,7 +266,7 @@ $(document).ready(function(){
 			success : function(response){
 				if(response.status == 1){
 					alert('Your Post has been deleted');
-					location.href() = '/'
+					window.location() = '/'
 				}else{
 					alert('Your Post could not be deleted')
 				}
